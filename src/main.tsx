ff21d78b9e6de56bbfa5fa8ef571845c9c719bbd
Dahaleez <daad.photography@gmail.com>
2026-04-29 14:20:16 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import AccessGate from './AccessGate';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+
+  <AccessGate>
+
     <App />
-  </StrictMode>,
+
+  </AccessGate>
+
+</StrictMode>
 );
